e38cfb66e5a5706fb0881991bf26e33caa9bbcee
Florian Renner <f.renner@zetes.at>
2020-05-11 10:42:13 +0200

    add types, listen to status events

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import RFIDScanner from './lib/RFIDScanner';
 import { RFIDScannerEvent } from './lib/RFIDScannerEvent';
+import { RFIDStatusEvent } from './lib/RFIDStatusEvent';
 
 export default RFIDScanner;
-export { RFIDScannerEvent };
+export { RFIDScannerEvent, RFIDStatusEvent };
